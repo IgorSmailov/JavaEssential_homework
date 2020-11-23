f4a86fd69f9705ff54185f48677149d136b40138
Igor Smailov <igor.ulianich@gmail.com>
2020-11-23 23:44:14 +0200

    Added additional task

---
+++ Java_Igor/src/Homework_02/Task02/Main.java
@@ -0,0 +1,13 @@
+package Homework_02.Task02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please add radius: =");
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+
+        System.out.println("Area of Circle: =" +MyArea.areaOfCircle(radius));
+    }
+}
---
+++ Java_Igor/src/Homework_02/Task02/MyArea.java
@@ -0,0 +1,9 @@
+package Homework_02.Task02;
+
+public class MyArea {
+    public static double areaOfCircle(double radius){
+    final double  PI = Math.PI;
+   double area = PI *(radius*radius);
+        return (area);
+   }
+}
