0841db1a674ef69e09ba9528fefe6d636727af3a
Igor Smailov <igor.ulianich@gmail.com>
2020-11-23 23:53:21 +0200

    Task1
    Updated task 1

--- Java_Igor/src/Homework_02/Task01/Car.java
+++ Java_Igor/src/Homework_02/Task01/Car.java
@@ -2,6 +2,14 @@ package Homework_02.Task01;
 
 public class Car {
     private int year;
-    private double speed;
-    private String colour;
+    private String  colour;
+
+    public Car(int year) {
+        this.year = year;
+    }
+
+    public Car(int year, String colour) {
+        this.year = year;
+        this.colour = colour;
+    }
 }
--- Java_Igor/src/Homework_02/Task01/Main.java
+++ Java_Igor/src/Homework_02/Task01/Main.java
@@ -2,6 +2,8 @@ package Homework_02.Task01;
 
 public class Main {
     public static void main(String[] args) {
-
+    Car myCar = new Car(2018);
+    Car myCar2 = new Car(2019, "Yellow");
+    Car myCar3 = new Car(2003);
     }
 }
