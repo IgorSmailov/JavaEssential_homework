0785e2701834d78acc136f749958a4c375a651ad
Igor Smailov <igor.ulianich@gmail.com>
2020-11-24 00:16:51 +0200

    Added Task4

--- Java_Igor/src/Homework_02/Task04/Car.java
+++ Java_Igor/src/Homework_02/Task04/Car.java
@@ -1,29 +1,32 @@
-package Homework_02.Task03;
+package Homework_02.Task04;
 
 public class Car {
+
     private int year;
     private double speed;
     private int weight;
     private String color;
 
+    public Car() {
+        this.year =2020;
+        this.speed = 260;
+        this.color = "Black";
+    }
 
     public Car(int year) {
         this.year = year;
     }
-    public Car (){
+
+    public Car(int year, double speed) {
+        this.year = 2016;
+        this.speed = speed;
     }
 
     public Car(int year, double speed, int weight) {
         this.year = year;
-        this.speed = speed;
-        this.weight = weight;
+        this.speed = 190;
+        this.weight = 1950;
     }
-
-    public Car(int year, double speed) {
-        this.year = year;
-        this.speed = speed;
-    }
-
 
     public Car(int year, double speed, int weight, String color) {
         this.year = year;
@@ -31,9 +34,4 @@ public class Car {
         this.weight = weight;
         this.color = color;
     }
-
-
-
-
-
 }
---
+++ Java_Igor/src/Homework_02/Task04/Main.java
@@ -0,0 +1,11 @@
+package Homework_02.Task04;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Car myCar1 = new Car(2020);
+        Car myCar2 = new Car(2016);
+        Car myCar3 = new Car(2020,130,1943);
+        Car myCar4 = new Car(2020,130,1999, "White");
+    }
+}
